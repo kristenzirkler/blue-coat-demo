8448dcc33106227161bfa6af42bc0f49d8ea1df3
Kristen Zirkler <bazirkler@gmail.com>
2015-02-02 18:41:42 -0700

    Simplified layout for beta

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 $(function(){
   
+  // Show Video
+  $('a').click(function() {
+  	event.preventDefault();
+    //$('#video-container').show('slow');
+    $('#video-container').addClass('open');
+    $('body').addClass('open-modal');
+  });
+  $('#video-container').click(function() {
+    //$('#video-container').hide('slow');
+    $('#video-container').removeClass('open');
+    $('body').removeClass('open-modal');
+
+  });
     
 });
