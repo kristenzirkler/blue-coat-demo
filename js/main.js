ab9e3b837715b6534e85707609fbadcea5356c27
Kristen Zirkler <bazirkler@gmail.com>
2015-04-15 14:10:00 -0700

    Updated videos added audio

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+  
   // Video Functionality
   $('.circle').click(function(event) {
     event.preventDefault();
@@ -6,11 +7,12 @@ $(function(){
 
     $('#video-container').addClass('open');
     $('body').addClass('open-modal');
+    $('.audioFrame').remove();
 
     if (videoID === '') {
       $('#video-player-ctr').html('No Video Found');
     } else {
-      $('#video-player-ctr').html('<video class="video-player" autoplay controls><source src="http://kristenzirkler.com/wp-content/uploads/video/'+videoID+'.mp4" type="video/mp4" /><source src="http://kristenzirkler.com/wp-content/uploads/video/'+videoID+'.webm" type="video/webm" />');
+      $('#video-player-ctr').html('<video class="video-player" autoplay controls><source src="http://104.236.213.243/wp-content/uploads/videos/'+videoID+'.mp4" type="video/mp4" /><source src="http://104.236.213.243/wp-content/uploads/videos/'+videoID+'.webm" type="video/webm" />');
     }
     
   });
@@ -19,7 +21,21 @@ $(function(){
     $('#video-player-ctr').html('Video');
     $('#video-container').removeClass('open');
     $('body').removeClass('open-modal');
+    addAudio();
 
   });
+  
+  // Voice Over
+  function addAudio() {
+    var aud = document.createElement('iframe');
+    aud.setAttribute('src', 'http://104.236.213.243/wp-content/uploads/videos/LifeCycle_VO.mp4'); // replace with actual file path
+    aud.setAttribute('width', '1px');
+    aud.setAttribute('height', '1px');
+    aud.setAttribute('class', 'audioFrame');
+    aud.setAttribute('scrolling', 'no');
+    aud.style.border = '0px';
+    document.body.appendChild(aud);
+  }
+  addAudio();
     
 });
